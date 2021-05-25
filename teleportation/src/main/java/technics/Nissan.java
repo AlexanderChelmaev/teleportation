bd6d98469e0952476c2c47562731b30e8f0c320a
a.chelmaev <a.chelmaev@infomaximum.com>
2021-05-25 17:12:16 +0300

    Добавил Nissan

---
+++ teleportation/src/main/java/technics/Nissan.java
@@ -0,0 +1,28 @@
+package technics;
+
+public class Nissan implements Car {
+
+    private int speed;
+    private String color;
+
+    public Nissan(int speed, String color) {
+        this.speed = speed;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
