4dc0b738053cc5de262c1b1c2d074bac6261bb39
a.chelmaev <a.chelmaev@infomaximum.com>
2021-05-25 17:37:36 +0300

    Renault WD

--- teleportation/src/main/java/technics/Renault.java
+++ teleportation/src/main/java/technics/Renault.java
@@ -3,6 +3,7 @@ package technics;
 public class Renault {
     private int speed;
     private String color;
+    private int WD;
 
     public Renault(int speed, String color) {
         this.speed = speed;
@@ -23,5 +24,13 @@ public class Renault {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getWD() {
+        return WD;
+    }
+
+    public void setWD(int WD) {
+        this.WD = WD;
     }
 }
