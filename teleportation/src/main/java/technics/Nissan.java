37498d4fb1bdde230789c1fe1a9d3f24422a1bf0
a.chelmaev <a.chelmaev@infomaximum.com>
2021-05-25 17:53:45 +0300

    Nissan WD

--- teleportation/src/main/java/technics/Nissan.java
+++ teleportation/src/main/java/technics/Nissan.java
@@ -4,6 +4,7 @@ public class Nissan implements Car {
 
     private int speed;
     private String color;
+    private int WD;
 
     public Nissan(int speed, String color) {
         this.speed = speed;
@@ -24,5 +25,13 @@ public class Nissan implements Car {
 
     public void setColor(String color) {
         this.color = color;
+    }
+
+    public int getWD() {
+        return WD;
+    }
+
+    public void setWD(int WD) {
+        this.WD = WD;
     }
 }
