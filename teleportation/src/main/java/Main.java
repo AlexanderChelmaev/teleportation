e6148e248b196f7bbbe13dc8f96c880c44bf190b
a.chelmaev <a.chelmaev@infomaximum.com>
2021-05-25 16:00:14 +0300

    Main -> print Cat and Dog

--- teleportation/src/main/java/Main.java
+++ teleportation/src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Learn GIT");
+        System.out.println("Learn GIT and Konsole");
+        System.out.println("Cat");
+        System.out.println("Dog");
     }
 }
