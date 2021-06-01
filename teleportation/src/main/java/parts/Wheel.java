a1e51555ecbd5cc36c54be1ad6cae5ff6d6d70b2
a.chelmaev <a.chelmaev@infomaximum.com>
2021-06-01 12:08:46 +0300

    Add Wheel

---
+++ teleportation/src/main/java/parts/Wheel.java
@@ -0,0 +1,11 @@
+package parts;
+
+public class Wheel {
+    private final int radius;
+    private final int season;
+
+    public Wheel(int radius, int season) {
+        this.radius = radius;
+        this.season = season;
+    }
+}
